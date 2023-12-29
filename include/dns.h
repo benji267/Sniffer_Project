c5e423a7ad111c12d2472a8eda01537f9e5789f7
Benjamin Metzger <benjamin.metzger.mpsi@gmail.com>
2023-12-29 16:30:17 +0100

    complete dns

--- include/dns.h
+++ include/dns.h
@@ -1,8 +1,7 @@
 #include "tcp.h"	
 #include "udp.h"
 #include <ctype.h>
-
-
+#include <stdbool.h>
 
 /**
  * @brief    print the class of the DNS packet for verbose mode 3
@@ -21,6 +20,15 @@ void print_classv3(uint16_t class);
 void print_typev3(const unsigned char *packet,uint16_t type);
 
 
+/**
+ * @brief     print the answer of the DNS packet for verbose mode 2
+ * 
+ * @param packet The packet to print
+ * @param type The type to print
+ * @param data_length The length of the data
+ * 
+ */
+void print_answer(const unsigned char* packet,uint16_t type,int data_length);
 
 
 /**
@@ -45,8 +53,9 @@ void print_typev2(uint16_t type);
  * 
  * @param packet The packet to print
  * @param verbose The verbose mode
+ * @param name The name of the DNS server
  */
-void dns_print_answers(const unsigned char *packet, int verbose);
+void dns_print_answers(const unsigned char *packet, int verbose,char* name);
 
 
 /**
@@ -54,9 +63,10 @@ void dns_print_answers(const unsigned char *packet, int verbose);
  *
  * @param packet  The packet to parse
  * @param verbose  Verbose mode
+ * @param answer  If the packet is an answer or not
  * 
  */
-void dns_print_queries(const unsigned char *packet, int verbose);
+void dns_print_queries(const unsigned char *packet, int verbose,bool answer);
 
 
 /**
@@ -65,9 +75,10 @@ void dns_print_queries(const unsigned char *packet, int verbose);
  * @param packet  The packet to parse
  * @param verbose  Verbose mode
  * @param MSB  The Most Significant Bit of QR
+ * @param answer  If the packet is an answer or not
  * 
  */
-void dns_print(const unsigned char *packet, int verbose,int MSB);
+void dns_print(const unsigned char *packet, int verbose,int MSB,bool answer);
 
 /**
  * @brief     get the Most Significant Bit of QR
--- src/dns.c
+++ src/dns.c
@@ -1,6 +1,153 @@
 #include "dns.h"
 
 
+
+void print_answer(const unsigned char* packet,uint16_t type,int data_length){
+    switch(type){
+        case 1:
+            printf("addr ");
+            for(int i=0;i<data_length;i++){
+                printf("%u",packet[i]);
+                if(i!=data_length-1){
+                    printf(".");
+                }
+            }
+            printf("\n");
+            break;
+        case 2:
+            printf("name server");
+            for(int i=0;i<data_length;i++){
+                if(isprint(packet[i])){
+                    printf("%c",packet[i]);
+                }
+                else{
+                    printf(".");
+                }
+            }
+            printf("\n");
+            break;
+        case 3:
+            printf("mail destination ");
+            for(int i=0;i<data_length;i++){
+                if(isprint(packet[i])){
+                    printf("%c",packet[i]);
+                }
+                else{
+                    printf(".");
+                }
+            }
+            printf("\n");
+            break;
+        case 4:
+            printf("mail forwarder ");
+            for(int i=0;i<data_length;i++){
+                if(isprint(packet[i])){
+                    printf("%c",packet[i]);
+                }
+                else{
+                    printf(".");
+                }
+            }
+            printf("\n");
+            break;
+        case 5:
+            printf("cname ");
+            for(int i=0;i<data_length;i++){
+                if(isprint(packet[i])){
+                    printf("%c",packet[i]);
+                }
+                else{
+                    printf(".");
+                }
+            }
+            printf("\n");
+            break;
+        case 6:
+            printf("primary name server ");
+            for(int i=0;i<data_length;i++){
+                if(isprint(packet[i])){
+                    printf("%c",packet[i]);
+                }
+                else{
+                    printf(".");
+                }
+            }
+            printf("\n");
+            break;
+        case 12:
+            printf("ptr ");
+            for(int i=0;i<data_length;i++){
+                if(isprint(packet[i])){
+                    printf("%c",packet[i]);
+                }
+                else{
+                    printf(".");
+                }
+            }
+            printf("\n");
+            break;
+        case 15:
+            printf("mail exchange ");
+            for(int i=0;i<data_length;i++){
+                if(isprint(packet[i])){
+                    printf("%c",packet[i]);
+                }
+                else{
+                    printf(".");
+                }
+            }
+            printf("\n");
+            break;
+        case 16:
+            printf("txt ");
+            for(int i=0;i<data_length;i++){
+                if(isprint(packet[i])){
+                    printf("%c",packet[i]);
+                }
+                else{
+                    printf(".");
+                }
+            }
+            printf("\n");
+            break;
+
+        case 28:
+            printf("addr ");
+            for(int i=0;i<data_length;i++){
+                printf("%x",packet[i]);
+                if(i!=data_length-1){
+                    printf(":");
+                }
+            }
+            printf("\n");
+            break;
+        case 251:
+            printf("ixfr ");
+            for(int i=0;i<data_length;i++){
+                printf("%x",packet[i]);
+                if(i!=data_length-1){
+                    printf(":");
+                }
+            }
+            printf("\n");
+            break;
+        case 252:
+            printf("axfr ");
+            for(int i=0;i<data_length;i++){
+                printf("%x",packet[i]);
+                if(i!=data_length-1){
+                    printf(":");
+                }
+            }
+            printf("\n");
+            break;
+        default:
+            printf("Unknown");
+            break;
+    }
+    return ;
+}
+
 void print_classv3(uint16_t class){
     switch(class){
         case 1:
@@ -89,7 +236,6 @@ void print_classv2(uint16_t class){
             printf("Unknown");
             break;
     }
-    printf("\n");
     return ;
 }
 
@@ -150,68 +296,102 @@ int getQR(unsigned char byte){
 }
 
 
-void dns_print_answers(const unsigned char *packet, int verbose){
-    
+void dns_print_answers(const unsigned char *packet, int verbose,char* name){
+    //I use packet_v3 to not modify the packet pointer and to treat the verbose level 3
+    const unsigned char *packet_v3=packet;
+    printf("type ");
+    packet++;
+    packet_v3++;
+    print_typev2(*packet);
+    printf("class ");
+    packet+=2;
+    print_classv2(*packet);
+    printf(", ");
+    packet+=5;
+    int length=packet[0]*256 + packet[1];
+    packet+=2;
+    print_answer(packet,*packet_v3,length);
+
 }
 
 
-void dns_print_queries(const unsigned char *packet, int verbose){
+void dns_print_queries(const unsigned char *packet, int verbose,bool answer){
     //I use packet_v3 to not modify the packet pointer and to treat the verbose level 3
     const unsigned char *packet_v3=packet;
-    printf("    ");
-    while(*packet!=0x00){
-        if(isprint(*packet)){
-            printf("%c",*packet);
-        }
-        else{
-            printf(".");
-        }
-        packet++;
-    }
+    //create a buffer to store the name
+    char* name;
 
-    printf(": type ");
-    print_typev2(packet[1]*256 + packet[2]);
-    printf("class ");
-    print_classv2(packet[3]*256 + packet[4]);
-    packet++;
-
-    if(verbose==3){
-        printf("        Name: ");
-        int length=0;
-        int label=1;
-        while(*packet_v3!=0x00){
-            if(isprint(*packet_v3)){
-                printf("%c",*packet_v3);
+    if(verbose>=2){
+        name=malloc(256);
+        printf("    "); 
+        int i=0;
+        if(!answer){
+            free(name);
+        }
+        printf("    ");
+        while(*packet!=0x00){
+            if(isprint(*packet)){
+                printf("%c",*packet);
+                if(answer){
+                    name[i]=*packet;
+                    i++;
+                }
             }
             else{
                 printf(".");
-                label++;
+                if(answer){
+                    name[i]='.';
+                    i++;
+                }
             }
-            length++;
+            packet++;
+        }
+        printf(": type ");
+        print_typev2(packet[1]*256 + packet[2]);
+        printf("class ");
+        print_classv2(packet[3]*256 + packet[4]);
+        printf("\n");
+        packet++;
+
+        if(verbose==3){
+            printf("        Name: ");
+            int length=0;
+            int label=1;
+            while(*packet_v3!=0x00){
+                if(isprint(*packet_v3)){
+                    printf("%c",*packet_v3);
+                }
+                else{
+                    printf(".");
+                    label++;
+                }
+                length++;
+                packet_v3++;
+            }
+            packet_v3++;
+            printf("\n");
+            printf("        [Name Length: %d]\n", length);
+            printf("        [Label Count: %d]\n", label);
+            printf("        Type: ");
+            packet_v3++;
+            print_typev3(packet_v3,*packet_v3);
+            printf("        Class: ");
+            packet_v3+=2;
+            print_classv3(*packet_v3);
             packet_v3++;
         }
-        packet_v3++;
-        printf("\n");
-        printf("        [Name Length: %d]\n", length);
-        printf("        [Label Count: %d]\n", label);
-        printf("        Type: ");
-        packet_v3++;
-        print_typev3(packet_v3,*packet_v3);
-        printf("        Class: ");
-        packet_v3+=2;
-        print_classv3(*packet_v3);
-        packet_v3++;
     }
-
-    if(*packet_v3==0xc0 && *(packet_v3+1)==0x0c){
-        packet+=6;
-        packet_v3+=2;
+    packet+=4;
+    if(*packet==0xc0 && *(packet+1)==0x0c && answer==true){
+        packet+=2;
         printf("    Answers\n");
+        printf("        %s: ", name);
+        dns_print_answers(packet, verbose, name);
     }
 
 }
 
-void dns_print(const unsigned char *packet, int verbose,int MSB){
+void dns_print(const unsigned char *packet, int verbose,int MSB, bool answer){
     printf("    Transaction ID: 0x%02x%02x\n", packet[0], packet[1]);
     printf("    Flags: 0x%02x%02x", packet[2], packet[3]);
     int flags=packet[2]*256 + packet[3];
@@ -371,9 +551,8 @@ void dns_print(const unsigned char *packet, int verbose,int MSB){
     //Before I don't do that to avoid to modify the packet pointer and to underline the constant length of the header
     //I add 13 not 12 to ignore the first byte of the first query 
     
-    if(verbose>2){
-        dns_print_queries(packet, verbose);
-    }
+    dns_print_queries(packet, verbose, answer);
+    
 
     return ;
 }
@@ -396,17 +575,18 @@ void dns(const unsigned char *packet, int verbose,int type, uint16_t* option_len
                     printf("Unknown\n");
                     break;
             }
-            
+            bool answ=false;
             int MSB=getQR(dns_packet[2]);
-            if(MSB==1){
+            if(MSB==0){
                 printf(" (query)\n");
             }
             else{
                 printf(" (response)\n");
+                answ=true;
             }
 
             if(verbose>1){
-                dns_print(dns_packet, verbose, MSB);
+                dns_print(dns_packet, verbose, MSB, answ);
             }
 
             break;
@@ -424,16 +604,18 @@ void dns(const unsigned char *packet, int verbose,int type, uint16_t* option_len
                     printf("Unknown\n");
                     break;
             }
+            bool answer=false;
             MSB=getQR(dns_packet[2]);
             if(MSB==0){
                 printf(" (query)\n");
             }
             else{
                 printf(" (response)\n");
+                answer=true;
             }
 
             if(verbose>1){
-                dns_print(dns_packet, verbose, MSB);
+                dns_print(dns_packet, verbose, MSB, answer);
             }
             break;
         default:
